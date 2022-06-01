2f0cea693dd54fa58acb5c7935966dec1c01bcc2
Adam Frederiksen <26208927+afrederiksen@users.noreply.github.com>
2022-06-01 04:27:50 -0400

    Dist

---
+++ dist/precache-manifest.cdb496d55e735a3b1ed6fc506e118b93.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "73bbd56ef3f18d1e7fcb",
+    "url": "/mecvision/css/app.f115216e.css"
+  },
+  {
+    "revision": "73bbd56ef3f18d1e7fcb",
+    "url": "/mecvision/js/app.96a0d534.js"
+  },
+  {
+    "revision": "a065a704bfc2b4cb2e1e",
+    "url": "/mecvision/css/chunk-vendors.42f86449.css"
+  },
+  {
+    "revision": "a065a704bfc2b4cb2e1e",
+    "url": "/mecvision/js/chunk-vendors.3904fad3.js"
+  },
+  {
+    "revision": "efdff29ece97fd76afef1e221c3173d9",
+    "url": "/mecvision/index.html"
+  },
+  {
+    "revision": "6a47843888fe188b9edba11f4b0f9a17",
+    "url": "/mecvision/img/bot.gif"
+  },
+  {
+    "revision": "d0aa14e8ed3363b70e50476f2138b54c",
+    "url": "/mecvision/img/bot.png"
+  },
+  {
+    "revision": "391e4fb8f4a4ec32c32ca20a566219a9",
+    "url": "/mecvision/img/legend.png"
+  },
+  {
+    "revision": "2cb4594fe5207412a8d15d751cf0e578",
+    "url": "/mecvision/img/type-magnified-thumb.jpg"
+  },
+  {
+    "revision": "20f7377552ea1611d841d097a87993b8",
+    "url": "/mecvision/img/type-paper-thumb.jpg"
+  },
+  {
+    "revision": "8e73b36270e282252a67944140bd53b8",
+    "url": "/mecvision/img/Screenshot-Descriptions.png"
+  },
+  {
+    "revision": "15664c7236991e795991aad4555b64ff",
+    "url": "/mecvision/img/type-microscope.jpg"
+  },
+  {
+    "revision": "54c697147c04fbc21bb150d77bd08da0",
+    "url": "/mecvision/img/type-microscope-thumb.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/mecvision/robots.txt"
+  },
+  {
+    "revision": "d3b374f148f42e909837a960a69a080a",
+    "url": "/mecvision/img/type-paper.jpg"
+  },
+  {
+    "revision": "068de0ecaf12ed0e41c933a56fd3433e",
+    "url": "/mecvision/img/type-magnified.jpg"
+  },
+  {
+    "revision": "8f9a45b13ada77d6b0f68f1a4efb29c9",
+    "url": "/mecvision/opencv.js"
+  }
+];
